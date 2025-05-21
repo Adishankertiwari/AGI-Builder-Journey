a43dbe41d767781d3a5711f6ee83962406adffe6
Adishankertiwari <astiwari301204@gmail.com>
2025-05-21 22:43:02 +0530

    Day 2 of python basics(txt to speech)

---
+++ day2_python_basics_pyttsx3installed.py
@@ -0,0 +1,5 @@
+#this code is for testing of pyttsx3 library(text to speech)
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Lingaanguleeguleegulee waashaa lingaangu lingaangu")
+engine.runAndWait()
